ee4c795be31fbab28e4f98a09a9101980a9c651d
Saad Alddine <saad.alam.sis@gmail.com>
2024-08-01 14:24:17 -0500

    created services for all entities

--- StudentPortal.BLL/StudentService.cs
+++ StudentPortal.BLL/StudentService.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StudentPortal.DAL;
+using StudentPortal.Models;
 
 namespace StudentPortal.BLL
 {
     public class StudentService
     {
+        private readonly StudentDAL _studentDAL;
+
+        public StudentService(StudentDAL studentDAL)
+        {
+            _studentDAL = studentDAL;
+        }
+
+        public List<Student> GetStudents()
+        {
+            return _studentDAL.GetStudents();
+        }
+
+        public Student GetStudent(int id)
+        {
+            return _studentDAL.GetStudent(id);
+        }
+
+        public void AddStudent(Student student)
+        {
+            _studentDAL.AddStudent(student);
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            _studentDAL.UpdateStudent(student);
+        }
+
+        public void DeleteStudent(int id)
+        {
+
+            _studentDAL.DeleteStudent(id);
+        }
+
+
+
     }
 }
