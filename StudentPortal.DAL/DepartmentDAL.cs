eb5abbfbd8e1aaa623b27f21f956236598cd75dc
Navjot Kaur <saininavjot357019@gmail.com>
2024-08-03 18:42:10 -0500

    Consistent indentation

--- StudentPortal.DAL/DepartmentDAL.cs
+++ StudentPortal.DAL/DepartmentDAL.cs
@@ -6,32 +6,39 @@ namespace StudentPortal.DAL
     {
         private readonly StudentPortalContext _context;
 
-        public DepartmentDAL(StudentPortalContext context) {
+        public DepartmentDAL(StudentPortalContext context) 
+        {
             _context = context;
         }
 
-        public List<Department> GetDepartments() {
+        public List<Department> GetDepartments() 
+        {
             return _context.Departments.ToList();
         }
 
-        public Department GetDepartment(int DepartmentID) {
+        public Department GetDepartment(int DepartmentID) 
+        {
             return _context.Departments.Find(DepartmentID);
         }
 
-        public void AddDepartment(Department department) {
+        public void AddDepartment(Department department) 
+        {
             _context.Departments.Add(department);
             _context.SaveChanges();
         }
 
 
-        public void UpdateDepartment(Department department) {
+        public void UpdateDepartment(Department department) 
+        {
             _context.Departments.Update(department);
             _context.SaveChanges();
         }
 
-        public void DeleteDepartment(int DepartmentID) {
+        public void DeleteDepartment(int DepartmentID) 
+        {
             var department = _context.Departments.Find(DepartmentID);
-            if (department != null) {
+            if (department != null) 
+            {
                 _context.Departments.Remove(department);
                 _context.SaveChanges();
             }
