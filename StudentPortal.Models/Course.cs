fe111f4c3df8af26a3958c8c754e2a7d476c47e2
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-07 15:56:47 -0500

    Fixed CRU Operations in Course

--- StudentPortal.Models/Course.cs
+++ StudentPortal.Models/Course.cs
@@ -7,7 +7,7 @@
         public string Duration { get; set; }
         public int Credits { get; set; }
         public int DepartmentID { get; set; }
-        public Department Department { get; set; }
-        public ICollection<Student> Students { get; set; }
+        public virtual Department Department { get; set; }
+        public ICollection<Student> Students { get; set; } = new List<Student>();
     }
 }
