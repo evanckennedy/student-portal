1aadf0f9b4732e09826a65609d1a7e405bb045f0
Navjot Kaur <saininavjot357019@gmail.com>
2024-08-08 21:04:37 -0500

    Styles for course create

--- StudentPortal.Models/CourseViewModel.cs
+++ StudentPortal.Models/CourseViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace StudentPortal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Models
 {
     public class CourseViewModel
     {
         public int CourseID { get; set; } = 0;
+        [Display(Name = "Course Name")]
         public string CourseName { get; set; }
         public string Duration { get; set; }
         public int Credits { get; set; }
--- StudentPortal.Models/StudentViewModel.cs
+++ StudentPortal.Models/StudentViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace StudentPortal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Models
 {
     public class StudentViewModel
     {
         public int StudentID { get; set; } = 0;
+        [Display(Name = "Student Name")]
         public string StudentName { get; set; }
+        [Display(Name = "Student Email")]
         public string StudentEmail { get; set; }
         public int DepartmentID { get; set; }
         public List<Department> Departments { get; set; } = new List<Department>();
