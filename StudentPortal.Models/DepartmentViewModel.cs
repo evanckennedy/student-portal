a00333f12325b8f7aef5c533a39105413fa19d32
Navjot Kaur <saininavjot357019@gmail.com>
2024-08-08 20:29:36 -0500

    Styles for department create

--- StudentPortal.Models/DepartmentViewModel.cs
+++ StudentPortal.Models/DepartmentViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace StudentPortal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Models
 {
     public class DepartmentViewModel
     {
+        [Display(Name = "Department Name")]
         public string DepartmentName { get; set; }
+        [Display(Name = "Department Head")]
         public string DepartmentHead { get; set; }
     }
 }
